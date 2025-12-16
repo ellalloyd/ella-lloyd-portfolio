dc04f286c06c3ee75e5cd873f6564018a6b972a2
Ella Lloyd <elloyd116@gmail.com>
2025-12-15 23:19:17 -0500

    functional projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,18 @@
 'use client'
 
-import { ProjectsBento } from "@/components/Projects";
+import { ProjectCard } from "@/components/ProjectCard";
+import { projects } from "@/lib/data";
 
 const ProjectsPage = () => {
     return (
-        <div className="flex justify-center items-center p-10">Content Coming Soon!</div>
+        <div className="flex flex-col gap-4 mx-auto my-8 p-4 rounded-4xl lg:max-w-3xl items-center justify-center ">
+            <h1 className="text-white text-2xl">Projects</h1>
+            <div className="flex gap-4 flex-col pb-0 ">
+                {projects.map((project) => (
+                    <ProjectCard key={project.id} project={project} />
+                ))}
+            </div>
+        </div>
 
     );
 };
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,7 +2,7 @@ import { Project } from "@/lib/types";
 
 function ProjectCard({ project }: { project: Project }) {
     return (
-        <div className="flex flex-col gap-2 min-w-[296px] lg:min-w-0 border border-background/50 rounded-2xl p-6 text-white  hover:bg-white hover:text-background cursor-pointer">
+        <div className="flex flex-col gap-2 min-w-[296px] max-w-[408px] lg:min-w-0 border border-background/50 rounded-2xl p-6 text-white  hover:bg-white hover:text-background cursor-pointer">
             <p className="font-semibold">{project.name}</p>
             <p className="text-[#d9d9d9]">{project.tools}</p>
             <p className="">{project.description}</p>
