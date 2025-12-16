0a7b1d3c90f375a3df842d53469cfa6df1905b18
Ella Lloyd <elloyd116@gmail.com>
2025-12-16 13:57:31 -0500

    styling updated

--- src/components/PowerliftingCard.tsx
+++ src/components/PowerliftingCard.tsx
@@ -5,10 +5,10 @@ import { ResponsiveContainer, LineChart, CartesianGrid, XAxis, YAxis, Tooltip, L
 
 function PowerliftingCard({ data }: { data: LiftingData[] }) {
     return (
-        <div className="flex flex-col gap-2 h-full">
+        <div className="flex flex-col lg:gap-2 gap-4 h-full">
             <p className="text-white text-2xl pb-2">Powerlifting</p>
-            <p className="text-[#d9d9d9] text-base">1 Rep Max Tracker</p>
-            <div className="flex flex-1 bg-white rounded-sm p-4 min-h-0 items-center justify-center">
+            <p className="text-[#d9d9d9]">1 Rep Max Tracker</p>
+            <div className="h-[436px] lg:flex-1 bg-white rounded-2xl p-4">
                 <ResponsiveContainer width="100%" height="100%">
                     <LineChart data={data} margin={{ top: 5, right: 30, bottom: 5, left: 0 }}>
                         <CartesianGrid strokeDasharray="3 3" />
@@ -16,9 +16,9 @@ function PowerliftingCard({ data }: { data: LiftingData[] }) {
                         <YAxis />
                         <Tooltip />
                         <Legend />
-                        <Line type="monotone" dataKey="squat" stroke="#6d597a" strokeWidth={2} name="Squat" />
-                        <Line type="monotone" dataKey="bench" stroke="#b8abc1" strokeWidth={2} name="Bench" />
-                        <Line type="monotone" dataKey="deadlift" stroke="#210534" strokeWidth={2} name="Deadlift" />
+                        <Line type="monotone" dataKey="squat" stroke="var(--chart-1)" strokeWidth={2} name="Squat" />
+                        <Line type="monotone" dataKey="bench" stroke="var(--chart-2)" strokeWidth={2} name="Bench" />
+                        <Line type="monotone" dataKey="deadlift" stroke="var(--chart-3)" strokeWidth={2} name="Deadlift" />
                     </LineChart>
                 </ResponsiveContainer>
             </div>
