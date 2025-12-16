a5a47de7dd08314b839f12d0426bbbfbc2aa32ef
Ella Lloyd <elloyd116@gmail.com>
2025-12-15 23:27:11 -0500

    added way back to home screen from projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,11 +2,18 @@
 
 import { ProjectCard } from "@/components/ProjectCard";
 import { projects } from "@/lib/data";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 const ProjectsPage = () => {
     return (
-        <div className="flex flex-col gap-4 mx-auto my-8 p-4 rounded-4xl lg:max-w-3xl items-center justify-center ">
-            <h1 className="text-white text-2xl">Projects</h1>
+        <div className="flex flex-col gap-4 mx-auto my-8 p-4 rounded-4xl w-fit items-center justify-center ">
+            <div className="relative flex flex-row items-center justify-center w-full text-white">
+                <Link href="/">
+                    <ArrowLeft className="absolute left-0 top-1/2 -translate-y-1/2 hover:size-8" />
+                </Link>
+                <h1 className="text-2xl">Projects</h1>
+            </div>
             <div className="flex gap-4 flex-col pb-0 ">
                 {projects.map((project) => (
                     <ProjectCard key={project.id} project={project} />
