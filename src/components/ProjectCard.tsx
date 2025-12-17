3bce0c2824beec57d7e036fa84f18d33dc9cc508
Ella Lloyd <elloyd116@gmail.com>
2025-12-17 12:05:11 -0500

    styling for clickableness

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -6,7 +6,7 @@ function ProjectCard({ project }: { project: Project }) {
 
     return (
         <Link href={`/projects/${project.id}`}>
-            <div className="flex flex-col gap-2 min-w-[296px] max-w-[408px] lg:min-w-0 border border-background/50 rounded-2xl p-6 text-white  hover:bg-white hover:text-background cursor-pointer">
+            <div className="flex flex-col gap-2 min-w-[296px] max-w-[408px] lg:min-w-0 border border-background/50 rounded-2xl p-6 text-white inset-shadow-background-500 inset-shadow-sm hover:bg-foreground transition-colors delay-150 duration-300 ease-in-out cursor-pointer">
                 <p className="font-semibold">{project.name}</p>
                 <p className="text-[#d9d9d9]">{project.tools}</p>
                 <p className="">{project.description}</p>
