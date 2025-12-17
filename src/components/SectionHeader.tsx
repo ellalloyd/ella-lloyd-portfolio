9a9fecd340739eea22c7cf827c8973214e7426e3
Ella Lloyd <elloyd116@gmail.com>
2025-12-17 11:54:49 -0500

    updated animation for arrow

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -6,7 +6,7 @@ function SectionHeader({ title, url }: { title: string, url: string }) {
         <div className="flex items-center justify-between w-full text-white pb-4">
             <p className="text-2xl">{title}</p>
             <Link href={url} >
-                <ArrowRight className="hover:size-8" />
+                <ArrowRight className="size-6 transition-transform duration-200 hover:scale-125" />
             </Link>
         </div>
     );
