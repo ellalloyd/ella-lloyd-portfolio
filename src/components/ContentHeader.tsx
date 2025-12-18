f1adfe16fa7eef3b730264f4cbd3df66f1fa54a8
Ella Lloyd <elloyd116@gmail.com>
2025-12-18 12:07:26 -0500

    spacing issue with content header

--- src/components/ContentHeader.tsx
+++ src/components/ContentHeader.tsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 function ContentHeader({ title, url }: { title: string | undefined, url: string }) {
     return (
         <div className="relative flex flex-row items-center justify-center w-full text-white">
-            <Link href={`/${url}`}>
+            <Link href={`/${url}`} className="flex">
                 <ArrowLeft className="absolute left-0 top-1/2 -translate-y-1/2 size-6 transition-transform duration-200 hover:scale-125" />
             </Link>
-            <h1 className="text-2xl">{title}</h1>
+            <h1 className="flex flex-1 text-2xl text-balance pl-8 lg:pl-10 text-left lg:justify-center">{title}</h1>
         </div>
     )
 }
