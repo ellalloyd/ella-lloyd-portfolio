ef384c204b23c5747932c64762a5fa29e7370b18
Ella Lloyd <elloyd116@gmail.com>
2025-12-18 11:16:40 -0500

    added mdx and reformatted projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,20 +1,14 @@
 'use client'
 
 import { ProjectCard } from "@/components/ProjectCard";
+import ContentHeader from "@/components/ProjectHeader";
 import { projects } from "@/lib/data";
-import { ArrowLeft } from "lucide-react";
-import Link from "next/link";
 
 const ProjectsPage = () => {
     return (
-        <div className="flex flex-col gap-4 mx-auto my-8 p-4 rounded-4xl w-fit items-center justify-center ">
-            <div className="relative flex flex-row items-center justify-center w-full text-white">
-                <Link href="/">
-                    <ArrowLeft className="absolute left-0 top-1/2 -translate-y-1/2 hover:size-8" />
-                </Link>
-                <h1 className="text-2xl">Projects</h1>
-            </div>
-            <div className="flex gap-4 flex-col pb-0 ">
+        <div className="flex flex-col gap-4 mx-auto my-8 p-4 rounded-4xl w-full lg:w-2/3 items-center justify-center">
+            <ContentHeader title="Projects" url="" />
+            <div className="flex gap-4 flex-col pb-0 w-fit">
                 {projects.map((project) => (
                     <ProjectCard key={project.id} project={project} />
                 ))}
